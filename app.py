ca0bdb3aac1e014f0fb48580dc4d8b4aba484b45
FWLSH <indservertripura@gmail.com>
2026-02-25 14:05:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ BACKGROUND_MODE = 'cover'           # choose 'cover' or 'contain'
 def fetch_player_info(uid: str):
     if not uid:
         return None
-    player_info_url = f"https://mrc-info.vercel.app/get_player_personal_show?server=ind&uid={uid}"
+    player_info_url = f"https://birthdayspecialinfoapi.vercel.app/player-info?uid={uid}"
     try:
         resp = session.get(player_info_url, timeout=IMAGE_TIMEOUT)
         resp.raise_for_status()
